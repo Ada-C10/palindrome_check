7b52f24e70e83e3bbf807d141a3926da1bbdfa1d
Aurea <fu.sakura@gmail.com>
2018-09-13 17:35:25 -0700

    implement and pass tests

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,38 @@
+require 'pry'
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  return false if my_phrase.nil?
+  phrase_nospaces = my_phrase.delete(' ')
+  return phrase_nospaces == reverse_array(phrase_nospaces)
+end
+
+
+
+def reverse_array(arr)
+
+  unless arr.nil?
+    length = arr.length
+    new_arr = copy_string(arr)
+
+    (length/2).times do |i|
+      tmp = new_arr[length - i - 1]
+      new_arr[length - i - 1] = new_arr[i]
+      new_arr[i] = tmp
+    end
+  end
+
+  return new_arr
+
+end
+
+def copy_string(arr)
+
+  new_arr = ""
+
+  arr.length.times do |i|
+    new_arr += arr[i]
+  end
+
+  return new_arr
 end
