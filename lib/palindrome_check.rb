51952f9b5372e6a739cfd4c5c9294e6f9d48274a
Hannah <hannahvanseeters@gmail.com>
2018-09-27 20:52:15 -0700

    palindrome complete with tests passing

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,25 @@
-# A method to check if the input string is a palindrome.
-# Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  if my_phrase.nil?
+    return false
+  end
+
+  start_counter = 0
+  end_counter = my_phrase.length - 1
+
+  until start_counter >= end_counter
+    while my_phrase[start_counter] == " "
+      start_counter += 1
+    end
+    while my_phrase[end_counter] == " "
+      end_counter -= 1
+    end
+    if my_phrase[start_counter] != my_phrase[end_counter]
+      return false
+    end
+
+    start_counter += 1
+    end_counter -= 1
+  end
+
+  return true
 end
