d77e312fd49f1a2f036921b0cd8c5bee70c111f4
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-02 19:27:02 -0700

    added space and time complexity explanations

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,9 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
+
+#The space complexity is O(1) because no space is alocated to create a new object
+#The time complexity is O(n) because the while loop needs to walk through each item in the string n times
+# (or technically O(n/2) times because two characters are checked with each loop through, but the 2 is dropped) 
 def palindrome_check(my_phrase)
   return false if my_phrase == nil
   # return true if my_phrase == ""
