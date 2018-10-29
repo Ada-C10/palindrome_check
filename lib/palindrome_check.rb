99b52efaf5e939147ac13773f00ee4874183c839
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-28 20:24:36 -0700

    edge cases not working

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,10 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
+
+require 'pry'
+
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  return false if my_phrase == nil
+  return true if (my_phrase.length == 1 || my_phrase == "")
+  return my_phrase == my_phrase.reverse
 end
