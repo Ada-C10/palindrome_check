10ad62723398b235d74a98ccd9875f3e48a0a11e
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-29 14:37:56 -0700

    palindrome passing, but with space consideration

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,34 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
-def palindrome_check(my_phrase)
-  raise NotImplementedError
+def string_reverse(my_string)
+  # my_string << "not implemented"
+  return nil if my_string == nil
+  return "" if my_string == ""
+
+  i = my_string.length - 1
+  j = 0
+
+  while j < i do
+    temp = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+    i -= 1
+    j += 1
+  end
+  return my_string
 end
+
+def palindrome_check(my_phrase)
+  # raise NotImplementedError
+  return false if my_phrase == nil
+  return true if my_phrase.length == 1
+
+  check = ""
+
+  puts check += my_phrase
+
+  return check == string_reverse(my_phrase) ? true : false
+end
+
+puts palindrome_check(" pull   up ")
+# puts palindrome_check("hannah")
