a2a59983beb15bd977c94c6c6b5acc260655848d
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-28 20:45:40 -0700

    tests passing

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -3,8 +3,36 @@
 
 require 'pry'
 
+# def palindrome_check(my_phrase)
+#   return false if my_phrase == nil
+#   return true if (my_phrase.length == 1 || my_phrase == "")
+#   return my_phrase == my_phrase.reverse
+# end
+
 def palindrome_check(my_phrase)
+
   return false if my_phrase == nil
-  return true if (my_phrase.length == 1 || my_phrase == "")
-  return my_phrase == my_phrase.reverse
+  return true if my_phrase.length <= 1
+
+  array = my_phrase.split(//)
+  temp_hash = {}
+
+  array.each do |char|
+
+    if temp_hash.include?char
+      temp_hash["#{char}"] += 1
+    else
+      temp_hash["#{char}"] = 1
+    end
+  end
+
+  odd_char = 0
+
+  temp_hash.each do |key, value|
+    if key != " " && value % 2 != 0
+      odd_char += 1 
+    end
+  end
+
+  return odd_char > 1 ? false : true
 end
