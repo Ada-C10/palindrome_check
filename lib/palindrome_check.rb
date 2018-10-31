f99fa164db8c306e0b138966d9e82801d8ec4274
Karis Kim <ouniiq@hotmail.com>
2018-10-30 17:45:04 -0700

    refactored code for a better solution

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -9,20 +9,33 @@ def palindrome_check(my_phrase)
   max = my_phrase.length - 1
   check = false
 
+# first check if string is empty or not
+# if not empty then compare the pair
+# if at least one of them is empty then move onto next index until it is not empty
+# then compare
+# if match continue moving up the index until min > max
+# if at least one pair that does not match then return false for good
 
 
-  until min == max
-    unless my_phrase[min] == " " || my_phrase[max] == " "
-      if my_phrase[min] == my_phrase[max]
-        check = true
-      end
+  until min > max
+    # unless my_phrase[min] == " " || my_phrase[max] == " "
+    until my_phrase[min] != " "
+      min += 1
     end
 
-    min += 1
-
-    if my_phrase[max] == " "
+    until my_phrase[max] != " "
       max -= 1
     end
+
+    if my_phrase[min] == my_phrase[max]
+      check = true
+    else
+      return false
+    end
+    # end
+
+    min += 1
+    max -= 1
     # require 'pry'; binding.pry
   end
 
