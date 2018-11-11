5ae6df291a6f1cc66be1e202a5c7373cbf24e079
Katricia Smith <smithkatricia@gmail.com>
2018-11-11 12:43:24 -0800

    Add method for palindrome check and start of helper delete space method

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,23 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+
+  index = 0
+  half_index = my_phrase.length / 2
+
+    until half_index
+      if my_phrase(index) != my_phrase(- (index +1))
+        return false
+      end
+    end
+
+  #single character, empty string,
+  return true
+end
+
+
+def delete_spaces(string)
+  string.each do |char|
+    
+  end
 end
