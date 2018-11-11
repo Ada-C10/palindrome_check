d04edb28f155f0445c6debaca101bdd90296e55b
Katricia Smith <smithkatricia@gmail.com>
2018-11-11 13:02:08 -0800

    Add method for palindrome check

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -2,22 +2,22 @@
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
 
+  return false if my_phrase == nil
+
+  my_phrase = my_phrase.delete(" ")
+
   index = 0
   half_index = my_phrase.length / 2
 
-    until half_index
-      if my_phrase(index) != my_phrase(- (index +1))
+    until index == half_index
+
+      if my_phrase[index] != my_phrase[- (index +1)]
         return false
+      else
+        index += 1
       end
     end
 
   #single character, empty string,
   return true
-end
-
-
-def delete_spaces(string)
-  string.each do |char|
-    
-  end
 end
