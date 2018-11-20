72cee0a5611dfc1b01cc5f3a4ad05841385de630
Alice Hsiao <alice.hsiao87@gmail.com>
2018-11-20 10:51:08 -0800

    Added test for string with only spaces, added restrictions to inner while loops, and updated space complexity

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,6 +1,6 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
-# Time Complexity: O(n/2) * [O(n) + O(n)] = O(n/2) * O(2n) = O(n^2)
+# Time Complexity: O(n) where n is the number of characters in input string. 
 # Space Complexity: O(1)
 
 def palindrome_check(my_phrase)
@@ -9,8 +9,8 @@ def palindrome_check(my_phrase)
   j = my_phrase.length - 1
 
   while i < j do
-    i += 1 while my_phrase[i] == " "
-    j -= 1 while my_phrase[j] == " "
+    i += 1 while my_phrase[i] == " " && i < j
+    j -= 1 while my_phrase[j] == " " && i < j
 
     return false if my_phrase[i] != my_phrase[j]
     i += 1
