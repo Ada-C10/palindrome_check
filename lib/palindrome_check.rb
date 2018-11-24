1f632cbbc3f19958c18c6b33e4017bf8e56849fc
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-24 13:57:23 -0800

    improved palindrome check

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -3,42 +3,39 @@
 def palindrome_check(my_phrase)
   # check for nil
   unless my_phrase.nil?
-    # remove all space from phrase
-    my_phrase = remove_spaces(my_phrase)
-    # call method to reverse the phrase
-     return check_pal(my_phrase)
+    i = 0
+    last = my_phrase.length - 1
+    while i < last # O(n/2)
+      # iterate from head of string until non whitespace char
+      until my_phrase[i] != ' ' # O(n/2)
+        i += 1
+      end
+
+      # iterate from trail of string until non whitespace char
+      until my_phrase[last] != ' ' # O(n/2)
+        last -= 1
+      end
+
+
+      # once two non-whitespace characters are found compare them
+      if my_phrase[i] != my_phrase[last]
+        return false
+      else
+        i += 1
+        last -= 1
+      end
+    end
+
+    return true
   end
 
   return false
-    # raise NotImplementedError
 end
 
-def remove_spaces(phrase)
-  i = 0
-  n = phrase.length
-  no_space_phrase = ''
-  while i < n
-    unless phrase[i] == ' '
-      no_space_phrase += phrase[i]
-    end
-    i += 1
-  end
-
-  return no_space_phrase
-end
-
-def check_pal(phrase)
-  i = 0
-  last = phrase.length - 1
-  flag = true
-  while i < last
-    if phrase[i] != phrase[last]
-      flag = false
-    end
-    i += 1
-    last -= 1
-  end
-  return flag
-end
-
-
+# For time complexity I think the overall number of operations is dependent on the
+# length of the string/phrase (n) At most 1/2 of the length of the string
+# operations will be necessary to iterate over (the head and tail
+# increment/decrement in unison)  O(n)
+# Using this method no additional space is required beyond the
+# head and tail incrementors as all non-whitespace characters are checked in
+# place and only a boolean is returned O(n)
